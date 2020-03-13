fc09341c01ddb0d99a5acac35293778c2de0c6cd
margaritageleta <noticiasmundiales99@gmail.com>
2020-03-13 18:26:03 +0100

    States and Props

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, PickerIOSComponent } from 'react-native';
+// useState to use States from React library
+// Sty;eSheet to use the CSS emulator 
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, PickerIOSComponent, Button } from 'react-native';
 
 export default function App() {
+
+  // Define a state and its update function in brackets
+  // useState hook, it gives the value to the state
+  const [name, setName] = useState('Client');
+  // Instead of a string, make an object  state
+  const [person, setPerson] = useState({name: 'John', age: '27'});
+
+  // Handler is a convention in React
+  const clickHandler = () => {
+    // update the name state
+    setName('New Client');
+    setPerson({name: 'Spurion', age: '23'})
+  }
+
   return (
     // View is a div
     <View style={styles.container}>
@@ -12,8 +28,18 @@ export default function App() {
       </View>
       <View style={styles.body}>
         <Text>
-          Hello again
+          Your name: {name}
         </Text>
+        <Text>
+          His name is {person.name} and his age is {person.age}
+        </Text>
+      </View>
+      <View style={styles.button}>
+        {/* Button is a self closing tag in React native
+        // So we use props to specify what text is gonna be in the button
+        // A prop is title 
+        // Another one is the onPress prop */}
+        <Button title='update name' onPress={clickHandler}/>
       </View>
     </View>
   );
@@ -40,5 +66,8 @@ const styles = StyleSheet.create({
     padding: 20,
     borderColor: 'black',
     borderWidth: 3,
+  },
+  button: {
+    marginTop: 20,
   },
 });
